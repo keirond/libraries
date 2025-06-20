4a70c88ae4282bed10cd6120d57c147bf3e5e3fa
Bao Dang <dang.hbaokody@gmail.com>
2025-06-20 15:52:22 +0700

    up Fri Jun 20 08:52:22 AM UTC 2025

--- configuration-loader/src/main/java/org/keiron/libraries/configuration/loader/Main.java
+++ configuration-loader/src/main/java/org/keiron/libraries/configuration/loader/Main.java
@@ -1,4 +1,4 @@
-package org.keiron.libraries;
+package org.keiron.libraries.configuration.loader;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
