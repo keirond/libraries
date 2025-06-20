d3a6598b77639e6579997119936fb2fa8e61ff35
Bao Dang <dang.hbaokody@gmail.com>
2025-06-20 15:11:43 +0700

    up Fri Jun 20 08:11:43 AM UTC 2025

--- kafka-performance-testing/src/main/java/org/keiron/libraries/cassandra/perfromance/testing/ConfigLoader.java
+++ kafka-performance-testing/src/main/java/org/keiron/libraries/cassandra/perfromance/testing/ConfigLoader.java
@@ -1,9 +1,5 @@
 package org.keiron.libraries.cassandra.perfromance.testing;
 
-import java.util.Properties;
-
 public class ConfigLoader {
-  public static Properties loadConfig(String configFile) {
-    Properties props = new Properties();
-  }
+
 }
