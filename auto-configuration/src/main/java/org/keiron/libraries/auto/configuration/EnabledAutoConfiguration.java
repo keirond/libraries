ca04994566516cf15103a5d6339632777dda78c3
Bao Dang <dang.hbaokody@gmail.com>
2025-06-22 22:47:31 +0700

    up Sun Jun 22 03:47:31 PM UTC 2025

--- auto-configuration/src/main/java/org/keiron/libraries/auto/configuration/EnabledAutoConfiguration.java
+++
@@ -1,7 +0,0 @@
-package org.keiron.libraries.auto.configuration;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface EnabledAutoConfiguration {}
