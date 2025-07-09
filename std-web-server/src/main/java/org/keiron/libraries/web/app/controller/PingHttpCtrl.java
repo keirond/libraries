a02e28b419df1cb0c729ffbfb66d1e82a6e85baf
Bao Dang <dang.hbaokody@gmail.com>
2025-07-09 14:23:06 +0700

    up Wed Jul  9 07:23:06 AM UTC 2025

--- std-web-server/src/main/java/org/keiron/libraries/web/app/controller/PingHttpCtrl.java
+++ std-web-server/src/main/java/org/keiron/libraries/web/app/controller/PingHttpCtrl.java
@@ -1,14 +1,17 @@
 package org.keiron.libraries.web.app.controller;
 
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.keiron.libraries.web.app.service.PingSvc;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Validated
 @RestController
 @RequestMapping(path = "/v1/ping")
 @RequiredArgsConstructor
@@ -17,7 +20,7 @@ public class PingHttpCtrl {
   private final PingSvc pingSvc;
 
   @GetMapping
-  public Mono<ResponseEntity<?>> pingRx(@RequestParam long at) {
+  public Mono<ResponseEntity<?>> ping(@RequestParam @Min(1) long at) {
     return pingSvc.ping(at).map(ResponseEntity::ok);
   }
 
